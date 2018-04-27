8b1e11c711e090d9137f855bc1d7ac100ffaee61
Anton Rylov <anton.rylov@propeople.com.ua>
2018-04-27 15:46:09 +0300

    Remove waste

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -2,10 +2,6 @@ module.exports = function() {
   $.gulp.task('pug', function() {
     return $.gulp.src('app/pug/pages/*.pug')
       .pipe($.plugins.pug({
-        // locals: {
-        //   nav: JSON.parse($.fs.readFileSync('./data/navigation.json', 'utf8')),
-        //   content: JSON.parse($.fs.readFileSync('./data/content.json', 'utf8'))
-        // },
         pretty: true
       }))
 
